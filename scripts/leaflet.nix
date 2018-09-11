354a345b4ad25427b41e509c9b71e6eef0bbc5ad
Matthew Pickering <matthewtpickering@gmail.com>
2018-09-11 09:03:41 +0000

    Update leaflet SHA

--- scripts/leaflet.nix
+++ scripts/leaflet.nix
@@ -7,7 +7,7 @@ let
                       src = np.fetchFromGitHub { owner = "mpickering"
                                                ; repo = "folium"
                                                ; rev = "d7bfd3e586b2e22e9bbd78feafe6e5005c98cd4f"
-                                               ; sha256 = "1qdhvqi7vagqbkh5v6lxmcv1ms5ixyll72v7qh05c1ans1prx4xa";
+                                               ; sha256 = "1176f42pag55i9d27z6pv755xs1z9b7hb1z5m880qva44sf0ybh2";
                                              }; }); };
 
   new-py = np.python3.override { packageOverrides = ext; };
