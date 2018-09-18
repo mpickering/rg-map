1b620ef863c66885a2b0779c4d5291ecdb62c6fd
Matthew Pickering <matthewtpickering@gmail.com>
2018-09-18 08:19:00 +0000

    Basic email sending

--- functions/send-verification-email/main.py
+++ functions/send-verification-email/main.py
@@ -1,3 +1,10 @@
+import sendgrid
+import os
+from sendgrid.helpers.mail import *
+
+
+
+
 def send_verification_email(data, context):
     """Background Cloud Function to be triggered by Cloud Storage.
        This generic function logs relevant data when a file is changed.
@@ -16,3 +23,14 @@ def send_verification_email(data, context):
     print('Metageneration: {}'.format(data['metageneration']))
     print('Created: {}'.format(data['timeCreated']))
     print('Updated: {}'.format(data['updated']))
+
+    sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
+    from_email = Email("maps@mpickering.github.io")
+    to_email = Email("matthewtpickering@gmail.com")
+    subject = "Sending with SendGrid is Fun"
+    content = Content("text/plain", "and easy to do anywhere, even with Python")
+    mail = Mail(from_email, subject, to_email, content)
+    response = sg.client.mail.send.post(request_body=mail.get())
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
