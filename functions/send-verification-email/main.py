9eb443fd5e3f2993f2b7df8bfb98e32b55f13d0e
Matthew Pickering <matthewtpickering@gmail.com>
2018-09-20 18:28:51 +0000

    Try using own tile server for static maps

--- functions/send-verification-email/main.py
+++ functions/send-verification-email/main.py
@@ -54,7 +54,8 @@ def send_verification_email(data, context):
     print('Updated: {}'.format(data['updated']))
 
 
-    map = staticmap.StaticMap(3000, 4000, 7)
+    map = staticmap.StaticMap(3000, 4000, zoom=14
+                             , url_template="https://storage.googleapis.com/gb-tiles/gb-tiles/{z}/{x}/{y}.png")
 
     with open("/tmp/map.jgw") as f:
         content = f.readlines()
