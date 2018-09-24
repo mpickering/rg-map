57c402f5b98efe16566574f3df5b46418758297c
Matthew Pickering <matthewtpickering@gmail.com>
2018-09-24 03:12:56 +0000

    Mark scraper as impure

--- src/Main.hs
+++ src/Main.hs
@@ -150,7 +150,7 @@ singleton x = [x]
 
 
 -- Need to mark this as impure
-scrape = nixScript [relfile|scraper.py|] [[relfile|shell.nix|]]
+scrape = impureNixScript [relfile|scraper.py|] [[relfile|shell.nix|]]
           (\() -> [ outParam ])
 
 fetch = nixScriptWithOutput [relfile|fetch.py|] [[relfile|shell.nix|]]
