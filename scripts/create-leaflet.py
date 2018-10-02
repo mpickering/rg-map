21ce3b157578e455322d3b0937660f066fe94362
Matthew Pickering <matthewtpickering@gmail.com>
2018-10-02 20:14:20 +0000

    Flag control

--- scripts/create-leaflet.py
+++ scripts/create-leaflet.py
@@ -10,12 +10,15 @@ import html
 import sys
 from osgeo import gdal, osr
 
+from branca.element import CssLink, Figure, JavascriptLink, MacroElement
+from jinja2 import Template
+import json
+
 
 vrt_dir = sys.argv[1]
 metadata_dir = sys.argv[2]
 zoom_level = int(sys.argv[3])
 output_dir = sys.argv[4]
-
 
 def make_event_link(event):
     split_url = urlsplit(event['map_url'])
@@ -85,14 +88,90 @@ def GetCenterImage(raster):
 
     return [geo_ext[0][1], geo_ext[0][0]]
 
+class FlagControl(MacroElement):
+    """
+    Adds a measurem widget on the map.
+    Parameters
+    ----------
+    position: location of the widget
+        default is 'topright'.
+    primary_length_unit and secondary_length_unit: length units
+         defaults are 'meters' and 'miles' respectively.
+    primary_area_unit and secondary_area_unit: ara units
+        defaults are 'sqmeters' and 'acres' respectively.
+    See https://github.com/ljagis/leaflet-measure for more information.
+    """
+    _template = Template("""
+        {% macro script(this, kwargs) %}
+            var {{this.get_name()}} = new L.easyButton({
+                  states: [{
+    stateName: 'set-flag',
+    icon: 'fa-flag',
+    title: 'Flag an incorrect map',
+    onClick: function(control, map) {
+      control.state('undo-flag');
+    }
+  }, {
+    icon: 'fa-undo',
+    stateName: 'undo-flag',
+    onClick: function(control, map) {
+      map.state = false;
+      control.state('set-flag');
+    },
+    title: 'reset flag'
+  }]})
+            {{this._parent.get_name()}}.addControl({{this.get_name()}});
+            {{this._parent.get_name()}}.flagControl = {{this.get_name()}};
+        {% endmacro %}
+        """)  # noqa
+
+
+    def __init__(self):
+        """Coordinate, linear, and area measure control"""
+        super(FlagControl, self).__init__()
+        self._name = 'FlagControl'
+
+        options = {
+        }
+        self.options = json.dumps(options)
+
+    def render(self, **kwargs):
+        super(FlagControl, self).render()
+
+        figure = self.get_root()
+        assert isinstance(figure, Figure), ('You cannot render this Element '
+                                            'if it is not in a Figure.')
+
+        figure.header.add_child(
+            JavascriptLink('https://cdn.jsdelivr.net/npm/leaflet-easybutton@2/src/easy-button.js'))  # noqa
+
+        figure.header.add_child(
+            CssLink('https://cdn.jsdelivr.net/npm/leaflet-easybutton@2/src/easy-button.css'))  # no})
+
 
 images = glob.glob(os.path.join(vrt_dir,'*.jpg.vrt'))
 
 print(len(images))
 
 
+flagControl = FlagControl()
+print(flagControl)
 m = folium.Map([54.3781, -3.4360], zoom_start=6, tiles='OpenStreetMap'
-              , prefer_canvas=True)
+              , prefer_canvas=True, flag_control=flagControl)
+flagControl.add_to(m)
+
+
+def flag_click(key):
+    return Template(
+        u"""function (e) {
+            if ({{m}}.flagControl._currentState.stateName == "undo-flag"){
+                jQuery.get(\"http://europe-west1-rg-maps-216117.cloudfunctions.net/flag_map?hash="""+key+u"""")
+                {{m}}.flagControl.state("set-flag")
+            }
+        }""")
+
+
+
 
 for image_vrt in images:
     print(image_vrt)
@@ -106,7 +185,8 @@ for image_vrt in images:
                  , popup=popup
                  , color=make_colour(event)
                  , fill_color=make_colour(event)
-                 , fill=True).add_to(m)
+                 , fill=True
+                 , onclick=flag_click(key)).add_to(m)
 
 tiles_loc = "https://s3-eu-west-1.amazonaws.com/rg-maps/{z}/{x}/{y}.png"
 tiles_loc_dev = "{z}/{x}/{y}.png"
