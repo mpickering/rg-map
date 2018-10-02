a0bf89e447a61d5c7eee36c552281c55f3e4deb5
Matthew Pickering <matthewtpickering@gmail.com>
2018-10-02 20:22:04 +0000

    Pin new version of folium

--- scripts/leaflet.nix
+++ scripts/leaflet.nix
@@ -4,11 +4,12 @@ let
   ext = py-self: py-super: {
           folium = py-super.folium.overridePythonAttrs ( oldAttrs : {
                       version = "0.6.0";
-                      src = np.fetchFromGitHub { owner = "mpickering"
-                                               ; repo = "folium"
-                                               ; rev = "d7bfd3e586b2e22e9bbd78feafe6e5005c98cd4f"
-                                               ; sha256 = "1176f42pag55i9d27z6pv755xs1z9b7hb1z5m880qva44sf0ybh2";
-                                             }; }); };
+            src = np.fetchFromGitHub { owner = "mpickering"
+                                     ; repo = "folium"
+                                     ; rev = "18d5acd71c089863c4008d8f2396297d41d4a00a"
+                                     ; sha256 = "1gd3y0s517l6j55n6rda2fqi7s2jark85y0vlkx1gi185cvx9sgf";
+          };
+  }); };
 
   new-py = np.python3.override { packageOverrides = ext; };
 
